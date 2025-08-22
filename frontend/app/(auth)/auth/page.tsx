a74714b8a218481bc2190d5d5238e18447deda04
harkirat <harkirat96@gmail.com>
2025-08-22 16:56:05 +0530

    Updated auth

--- frontend/app/(auth)/auth/page.tsx
+++ frontend/app/(auth)/auth/page.tsx
@@ -8,10 +8,10 @@ function LoginPage() {
   const [email, setEmail] = useState("");
   const [step, setStep] = useState("email");
   return (
-    <>
+    <div className="flex h-screen w-screen items-center justify-center">
       {step === "email" && <Email setEmail={setEmail} setStep={setStep} email={email} />}
       {step === "otp" && <Otp email={email} />}
-    </>
+    </div>
   );
 }
 
