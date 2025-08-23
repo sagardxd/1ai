7e5606bbdc5b5038f3a4ff76d77f10bbe414e1b8
harkirat <harkirat96@gmail.com>
2025-08-24 01:51:51 +0530

    increased rate limits

--- backend/ratelimitter.ts
+++ backend/ratelimitter.ts
@@ -4,13 +4,13 @@ import rateLimit from "express-rate-limit";
 // per-minute limiter
 export const perMinuteLimiter = rateLimit({
   windowMs: 60 * 1000, // 1 minute
-  max: 2,
+  max: 30,
   message: "Too many requests. Try again in a minute.",
 });
 
 // per-hour limiter
 export const perMinuteLimiterRelaxed = rateLimit({
   windowMs: 60 *  1000, // 1 hour
-  max: 5,
+  max: 100,
   message: "Too many requests. Try again in an hour.",
 });
